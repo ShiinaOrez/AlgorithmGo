bd3ac6c2c8fade15aebd9968767a4bbed36473da
ShiinaOrez <shiina_orez@qq.com>
2019-01-23 00:26:02 +0800

    feat: union find set

---
+++ unionfindset/unionfindset.go
@@ -0,0 +1,20 @@
+package unionfindset
+
+type UnionFindSet struct {
+    Fa    []int
+}
+
+func (set *UnionFindSet) Union(x, y int) {
+    set.Fa[set.Find(x)] = y
+    return
+}
+
+func (set *UnionFindSet) Find(v int) int{
+    u := v
+    if set.Fa[u] == u {
+        set.Fa[v] = set.Find(set.Fa[u])
+    } else {
+        return u
+    }
+    return set.Fa[v]
+}
