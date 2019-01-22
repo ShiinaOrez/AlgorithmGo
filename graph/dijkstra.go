36678373eca5625b43fa9cfe2dc4b9663bcf49e5
ShiinaOrez <shiina_orez@qq.com>
2019-01-23 01:02:22 +0800

    init: graph dijkstra algorithm

---
+++ graph/dijkstra.go
@@ -0,0 +1,19 @@
+package graph
+
+func (index Type) getNode(g *Graph) *GraphNode {
+	return g.NodeTree.Find(int(index))
+}
+
+func (node *GraphNode) getNode(g *Graph) *GraphNode {
+	return node
+}
+
+type DijkstraArg interface {
+	getNode( *Graph)   *GraphNode
+}
+
+func (g *Graph) Dijkstra(from, to DijkstraArg) Type {
+	source := from.getNode(g)
+	target := to.getNode(g)
+	
+}
