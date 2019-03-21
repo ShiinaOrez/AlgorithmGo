d92a639632bcc84029dc93634571870870260fef
ShiinaOrez <shiina_orez@qq.com>
2019-03-21 16:10:56 +0800

    [update]tree

--- tree/tree.go
+++ tree/tree.go
@@ -29,6 +29,8 @@ func CreateTree() *Tree {
     return tree
 }
 
+// SetRootNode
+// @Description: Set a node to tree's root node, please make sure node belong to this tree
 func (tree *Tree) SetRootNode(node *Node) error {
     if node.Belong == tree{
         tree.Root = node
@@ -38,15 +40,59 @@ func (tree *Tree) SetRootNode(node *Node) error {
     return errors.New("[Error]: Node must in this tree!")
 }
 
+// JoinTree
+// @Description: Join a node to a tree, if node used belong another tree, it will delete it.
 func (node *Node) JoinTree(tree *Tree) error {
     if node.Belong == tree {
         return errors.New("[Error]: Node already in this tree!")
     } else {
         if node.Belong != nil {
-            node.Belong.Size -= 1
+            err := node.Belong.DelNode(node)
+            if err != nil {
+                return errors.New("[Error]: Delete node from tree failed!")
+            }
         }
         node.Belong = tree
         tree.Size += 1
     }
     return nil
+}
+
+func (tree *Tree) DelNode(node *Node) error {
+    if node.Belong != tree {
+        return errors.New("[Error]: Before you delnode from a tree, please make sure node belong it.")
+    } else {
+        tree.Size -= 1
+        err := node.CleanLinks()
+        if err != nil {
+            return errors.New("[Error]: Clean node's link failed")
+        }
+    }
+    return nil
+}
+
+func (node *Node) CleanLinks() error {
+    node.LinkNodes = nil
+    return nil
+}
+
+func (node *Node) LinkTo(to *Node) error {
+    if node.Belong == to.Belong && node.Belong != nil {
+        if to.In(node.LinkNodes) {
+
+        }
+    } else if node.Belong == nil || to.Belong == nil {
+        return errors.New("[Error]: Nodes must belong to a tree.")
+    } else if node.Belong != to.Belong {
+        return errors.New("[Error]: Nodes must belong to a same tree!")
+    }
+}
+
+func (node *Node) In(sli []*Node) bool{
+    for _, per := range sli {
+        if per == node {
+            return true
+        }
+    }
+    return false
 }
