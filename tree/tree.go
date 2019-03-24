74d4817777d578357f97454c34154557a75bfc6a
ShiinaOrez <shiina_orez@qq.com>
2019-03-24 13:01:41 +0800

    [update]tree.DFS

--- tree/tree.go
+++ tree/tree.go
@@ -113,8 +113,38 @@ func (node *Node) Range(src []*Node, from *Node) []*Node {
     return src
 }
 
-func (tree *Tree) Range() []*Node {
+func (tree *Tree) Range() ([]*Node, error) {
     var res []*Node
+    if tree.Root == nil {
+        return nil, errors.New("[Error]: Tree must have a root!")
+    }
     res = tree.Root.Range(res, nil)
-    return res
+    return res, nil
+}
+
+func (tree *Tree) DFStep(now , from *Node, res []*Node) ([]*Node, error) {
+    if now.Belong != tree {
+        return nil, errors.New("[Error]: DFS now node must belong to this tree!")
+    } else if from != nil && from.Belong != tree {
+        return nil, errors.New("[Error]: DFS from node must belong to this tree!")
+    } else {
+        res = append(res, now)
+        for _, nod := range now.LinkNodes {
+            if from == nod {
+                continue
+            } else {
+                var err error
+                res, err = tree.DFStep(nod, now, res)
+                if err != nil {
+                    return nil, err
+                }
+            }
+        }
+    }
+    return res, nil
+}
+
+func (tree *Tree) DFS(now , from *Node) ([]*Node, error) {
+    var res []*Node
+    return tree.DFStep(now, from, res)
 }
